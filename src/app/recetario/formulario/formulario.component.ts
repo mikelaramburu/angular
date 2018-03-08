bc163788d7d85b8eccce3e31de4b0fb2ae1b1c8d
Curso <Curso@IPARTEKAULA.COM>
2018-03-08 11:26:51 +0100

    MTX8_modal recetas terminado. Falta maquetar concesionario.html

--- src/app/recetario/formulario/formulario.component.ts
+++ src/app/recetario/formulario/formulario.component.ts
@@ -6,7 +6,7 @@ import { Receta } from '../../model/receta';
 //     npm install --save -dev jquery         */
 //******************************************* */
 import * as $ from 'jquery';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -16,12 +16,13 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 export class FormularioComponent implements OnInit {
   formulario: FormGroup;
-
+  ingredientes : FormArray;
   
 
   constructor (private fb:FormBuilder, public recetasService: RecetasService) { 
     console.log("FormularioComponent");
     this.crearFormulario();
+    this.ingredientes = this.formulario.get('ingredientes') as FormArray;
   }
 
   ngOnInit() {
@@ -34,40 +35,90 @@ export class FormularioComponent implements OnInit {
           //FormControl (input) =>['value',[Validaciones]]
           nombre:['', [Validators.required, Validators.minLength(2)]],
           cocinero:['', [Validators.minLength(5)]],
-          imagen: ['/assets/img/receta_default.jpg'],
-          descripcion:['',[Validators.minLength(100)]]
+          imagen: ['/assets/img/receta_default.jpg', Validators.required],
+          descripcion:['',[Validators.minLength(100)]],
+          gluten:["true", Validators.required],
+          ingredientes : this.fb.array([ this.createIngredienteFormGroup() ])
 
     });
 
   }
 
+  createIngredienteFormGroup(): FormGroup {
+    console.log('FormularioComponent createIngredienteFormGroup');
+    return this.fb.group({
+      nombre: ['', [Validators.required]]
+    });
+  }
+
+  /** 
+   * Evento para crear un nuevo Ingrediente
+  */
+ clickOtroIngrediente(){
+  console.log('FormularioComponent clickOtroIngrediente');    
+  this.ingredientes.push( this.createIngredienteFormGroup() );
+}
+
+clickEliminarIngrediente( index ){
+  console.log('FormularioComponent clickEliminarIngrediente');    
+  this.ingredientes.removeAt(index);
+}
+
   submitar(): void{
     console.log("FormularioComponent onSubmit");
-      
-    //TO DO recoger datos formulario
-    let nombre = this.formulario.value.nombre;
-
-    let cocinero = this.formulario.value.cocinero;
-    if (cocinero === '') {
-      cocinero = 'Anonimo';
-    }
-
-    let img = this.formulario.value.imagen;
-
-    let isGlutenFree: boolean = false;
-    if (this.formulario.value.gluten === '1') {
-      isGlutenFree = true;
-    }
-
-    let descripcion = this.formulario.value.descripcion;
-
-    let receta: Receta = new Receta( nombre, img, descripcion, cocinero);
-    this.recetasService.crear(receta);
-
     
-     //cerrar Modal
+    //recoger datos del formulario
+    let receta = this.mapearFormularioReceta(this.formulario);
+   
+    //llamar Servicio
+    this.recetasService.crear( receta );
+
+    //limpiar Formulario y poner un solo ingrediente
+    this.formulario.reset();
+    this.crearFormulario();
+    this.ingredientes.controls.splice(1);
+
+
+    //TODO cerrar modal    
+    //$("#modalReceta").modal('hide');
     $("#btn-close").click();
 
   }
+   /**
+   * Nos retorna las clases para darle estilos al div que contiene el input
+   * @param control : FormControl
+   */
+  estilosInput( control : FormControl ):string{
+
+    const CLASS_ERROR = "form-group has-error";
+    const CLASS_SUCCESS = "form-group has-success";
+
+    if ( control.dirty ){
+        return (control.valid)?CLASS_SUCCESS:CLASS_ERROR;
+    }else{
+      return "form-group";
+    }
+  }
+
+  /**
+   * Mapear Los datos del Formulario a una Receta
+   * @param form : FormGroup
+   */
+   mapearFormularioReceta( form : FormGroup ):Receta{
+      
+      let receta = new Receta(form.value.nombre, form.value.cocinero, form.value.imagen, form.value.ingredientes);
+      receta.cocinero = form.value.cocinero;
+      receta.isGlutenFree = ( form.value.gluten === "true" )?false:true;
+      receta.imagen = form.value.imagen;
+      receta.descripcion = form.value.descripcion;
+
+      //recuperar los ingredientes
+      form.value.ingredientes.map(element => {
+        receta.addIngredientes( element.nombre );
+      });
+
+      return receta;
+   }
+
 
 }
